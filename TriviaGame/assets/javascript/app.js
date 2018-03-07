a01a35ee7cae32b8c130c61a621c153d8e5f94ae
Mandolin77 <31786869+Mandolin77@users.noreply.github.com>
2018-03-07 00:00:59 -0600

    Add files via upload

---
+++ TriviaGame/assets/javascript/app.js
@@ -0,0 +1,38 @@
+window.onload = function() {
+
+
+
+    var questionsAndAnswers = {
+        Question1: ["choice1", "choice2", "choice3", "choice4"],
+        Question2: ["choice1", "choice2", "choice3", "choice4"],
+        Question3: ["choice1", "choice2", "choice3", "choice4"],
+        Question4: ["choice1", "choice2", "choice3", "choice4"],
+        Question5: ["choice1", "choice2", "choice3", "choice4"],
+        Question6: ["choice1", "choice2", "choice3", "choice4"],
+        Question7: ["choice1", "choice2", "choice3", "choice4"],
+        Question8: ["choice1", "choice2", "choice3", "choice4"],
+        Question9: ["choice1", "choice2", "choice3", "choice4"],
+        Question10: ["choice1", "choice2", "choice3", "choice4"],
+    }
+
+//start game on click
+
+//set timer for 20 seconds
+//load question 1 in box 1
+//load answers for question 1
+//when click answer,
+    //if correct, say correct for 3 seconds
+    //add 1 correct to tally
+    //automatically load next question
+
+    //if incorrect, say correct answer for 3 seconds
+    //add 1 incorrect to tally
+    //automatically load next question
+
+//when cycle through all questions, after 3 seconds load game completed page
+    //show how many correct, how many incorrect
+
+
+
+
+}
